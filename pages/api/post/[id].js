f610a1d810c90762fae5484cfa7aab7ce6913fce
Grant Johnson <grantlj@comcast.net>
2022-12-19 17:24:28 -0500

    More errors

---
+++ pages/api/post/[id].js
@@ -0,0 +1,11 @@
+import dbConnect from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+    const { method } = req;
+    await dbConnect();
+    switch (method) {
+        default:
+            res.status(400).end();
+            break;
+    }
+}
--- pages/post/create.js
+++ pages/post/create.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CreatePost from "../../components/CreatePost";
 
-export default function create() {
+export default function Create() {
     return <CreatePost />;
 }
